6b8c6952dd5f331b04d56530e2908aaa447d0169
Samuel Souza <smksouza.dev@gmail.com>
2023-11-02 17:16:05 -0300

    fix: migrate config

--- database/migrations/20231102183717_create-users.ts
+++ database/migrations/20231102183717_create-users.ts
@@ -6,6 +6,7 @@ export async function up(knex: Knex): Promise<void> {
     table.text('name').notNullable()
     table.text('email').notNullable()
     table.text('password').notNullable()
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
   })
 }
 
