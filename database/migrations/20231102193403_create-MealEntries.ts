38fe61df318d5c1e9e485c9e98510eefc5a665b8
Samuel Souza <smksouza.dev@gmail.com>
2023-11-04 10:14:57 -0300

    .env: created

--- database/migrations/20231102193403_create-MealEntries.ts
+++ database/migrations/20231102193403_create-MealEntries.ts
@@ -3,7 +3,12 @@ import { Knex } from 'knex'
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('meals', (table) => {
     table.increments('id').primary()
-    table.uuid('user_id').index()
+    table
+      .uuid('user_id')
+      .index()
+      .references('id')
+      .inTable('users')
+      .onDelete('CASCADE')
     table.text('name').notNullable()
     table.text('description')
     table.boolean('IsOnDiet')
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,18 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  const users = await knex('users').where('name', 'Samuel Pereira').select('*')
 
-  return tables
+  return users
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP Server Running')
